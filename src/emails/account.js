f9164da2666bcaa7b3a0fb5d978b5df2ad391fd5
Immanuel Kolapo <immanuelcaspian@gmail.com>
2020-02-25 11:02:46 +0100

    Changed the from email address

--- src/emails/account.js
+++ src/emails/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'icaspian@outlook.com',
+        from: 'immanuelcaspian@gmail.com',
         subject: 'Welcome aboard!',
         text: `Hey ${name}, thanks for joining in! Let me know how you get along with the app.`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'icaspian@outlook.com',
+        from: 'immanuelcaspian@gmail.com',
         subject: `Goodbye, ${name}!`,
         text: `We are sad to see you go ${name}. Let us know what we could have done better. Feel free to come back to us!`
     })
