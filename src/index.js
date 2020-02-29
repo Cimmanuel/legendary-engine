961bc4562fd2fbaa0d2631203cecd734db328495
Immanuel Kolapo <immanuelcaspian@gmail.com>
2020-02-29 21:24:09 +0100

    Refactored for testing purposes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-const express = require('express')
-const app = express()
-
-require('./db/mongoose')
-
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
+const app = require('./app') 
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is running on port', port)
 })
+
+
+// const express = require('express')
+// const app = express()
+
+// require('./db/mongoose')
+
+// const userRouter = require('./routers/user')
+// const taskRouter = require('./routers/task')
+
+// const port = process.env.PORT
+
+// app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
+
+// app.listen(port, () => {
+//     console.log('Server is running on port', port)
+// })
